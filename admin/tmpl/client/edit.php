2aaf5e99b1d6c3e0126493dbad1ae2084583a241
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-02-25 22:55:02 -0800

    Update edit.php

--- admin/tmpl/client/edit.php
+++ admin/tmpl/client/edit.php
@@ -32,66 +32,32 @@ $listDirn  = $this->escape($this->state->get('list.direction'));
 ?>
 
 <form action="<?php echo Route::_('index.php?option=com_mothership&layout=edit&id=' . (int) $this->item->id); ?>" method="post" name="adminForm" id="client-form" aria-label="<?php echo Text::_('COM_MOTHERSHIP_CLIENT_' . ((int) $this->item->id === 0 ? 'NEW' : 'EDIT'), true); ?>" class="form-validate">
-
-    <?php echo LayoutHelper::render('joomla.edit.title_alias', $this); ?>
-
     <div class="main-card">
         <?php echo HTMLHelper::_('uitab.startTabSet', 'myTab', ['active' => 'details', 'recall' => true, 'breakpoint' => 768]); ?>
 
-        <?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'details', Text::_('COM_MOTHERSHIP_CLIENT_DETAILS')); ?>
+        <?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'details', Text::_('COM_MOTHERSHIP_FORM_CLIENT_DETAILS_TAB')); ?>
         <div class="row">
             <div class="col-lg-9">
-                <?php echo $this->form->renderField('type'); ?>
-                <div id="image">
-                    <?php echo $this->form->renderFieldset('image'); ?>
+                <div>
+                    <fieldset class="adminform">
+                    <?php echo $this->form->renderField('name'); ?>
+                    <?php echo $this->form->renderField('contact'); ?>
+                    <?php echo $this->form->renderField('email'); ?>
+                    <?php echo $this->form->renderField('phone'); ?>
+                    </fieldset>
                 </div>
-                <div id="custom">
-                    <?php echo $this->form->renderField('custombannercode'); ?>
-                </div>
-                <?php
-                echo $this->form->renderField('clickurl');
-                echo $this->form->renderField('description');
-                ?>
             </div>
             <div class="col-lg-3">
-                <?php echo LayoutHelper::render('joomla.edit.global', $this); ?>
+                <?php echo $this->form->renderField('default_rate'); ?>
             </div>
         </div>
-        <?php echo HTMLHelper::_('uitab.endTab'); ?>
 
-        <?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'otherparams', Text::_(' COM_MOTHERSHIP_GROUP_LABEL_BANNER_DETAILS')); ?>
-            <fieldset id="fieldset-otherparams" class="options-form">
-                <legend><?php echo Text::_(' COM_MOTHERSHIP_GROUP_LABEL_BANNER_DETAILS'); ?></legend>
-                <div>
-                    <?php echo $this->form->renderFieldset('otherparams'); ?>
-                </div>
-            </fieldset>
-        <?php echo HTMLHelper::_('uitab.endTab'); ?>
-
-        <?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'publishing', Text::_('JGLOBAL_FIELDSET_PUBLISHING')); ?>
-        <div class="row">
-            <div class="col-md-6">
-                <fieldset id="fieldset-publishingdata" class="options-form">
-                    <legend><?php echo Text::_('JGLOBAL_FIELDSET_PUBLISHING'); ?></legend>
-                    <div>
-                        <?php echo LayoutHelper::render('joomla.edit.publishingdata', $this); ?>
-                    </div>
-                </fieldset>
-            </div>
-            <div class="col-md-6">
-                <fieldset id="fieldset-metadata" class="options-form">
-                    <legend><?php echo Text::_('JGLOBAL_FIELDSET_METADATA_OPTIONS'); ?></legend>
-                    <div>
-                    <?php echo $this->form->renderFieldset('metadata'); ?>
-                    </div>
-                </fieldset>
-            </div>
-        </div>
         <?php echo HTMLHelper::_('uitab.endTab'); ?>
 
         <?php echo HTMLHelper::_('uitab.endTabSet'); ?>
     </div>
 
-    <input type="hidden" name="task" value="">
-    <?php echo HTMLHelper::_('form.token'); ?>
+    <input type="hidden" name="jform[id]" value="<?php echo (isset($this->item->id) && $this->item->id > 0) ? (int) $this->item->id : ""; ?>" />
+    <input type="hidden" name="task" value="" />
+    <?php echo JHtml::_('form.token'); ?>
 </form>
