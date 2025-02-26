0d0f2442cbf34747eeb76a5b2220d69d6ace7a82
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-02-25 20:28:14 -0800

    Update provider.php

--- admin/services/provider.php
+++ admin/services/provider.php
@@ -2,6 +2,7 @@
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Factory;
 use Joomla\CMS\Dispatcher\ComponentDispatcherFactoryInterface;
 use Joomla\CMS\Extension\ComponentInterface;
 use Joomla\CMS\Extension\MVCComponent;
@@ -11,11 +12,19 @@ use Joomla\CMS\MVC\Factory\MVCFactoryInterface;
 use Joomla\DI\Container;
 use Joomla\DI\ServiceProviderInterface;
 
+/**
+ * Joomla 5 Service Provider for com_mothership
+ */
 return new class implements ServiceProviderInterface {
-    
+
     public function register(Container $container): void {
+        // Register MVC Factory
         $container->registerServiceProvider(new MVCFactory('\\TrevorBice\\Component\\Mothership'));
+
+        // Register Component Dispatcher
         $container->registerServiceProvider(new ComponentDispatcherFactory('\\TrevorBice\\Component\\Mothership'));
+
+        // Register Component Interface
         $container->set(
             ComponentInterface::class,
             function (Container $container) {
@@ -24,6 +33,24 @@ return new class implements ServiceProviderInterface {
 
                 return $component;
             }
+        );
+
+        // ✅ Use Factory::getApplication() to register assets
+        $app = Factory::getApplication();
+        $wa = $app->getDocument()->getWebAssetManager();
+
+        // ✅ Register the JavaScript file properly
+        $wa->registerScript(
+            'com_mothership.client-edit',
+            'administrator/components/com_mothership/assets/js/client-edit.js',
+            [],
+            ['defer' => true]
+        );
+
+        // ✅ Optional: Register CSS file if needed
+        $wa->registerStyle(
+            'com_mothership.client-edit',
+            'administrator/components/com_mothership/assets/css/client-edit.css'
         );
     }
 };
