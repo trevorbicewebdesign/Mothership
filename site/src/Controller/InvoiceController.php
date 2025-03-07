680e8161e9843589a844e4266a996ed40c01eee7
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-03-07 11:51:56 -0800

    Update InvoiceController.php

--- site/src/Controller/InvoiceController.php
+++ site/src/Controller/InvoiceController.php
@@ -9,6 +9,8 @@ use Joomla\CMS\MVC\Controller\BaseController;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Layout\FileLayout;
 use Mpdf\Mpdf;
+use Joomla\Event\DispatcherInterface;
+use Joomla\Event\Event;
 
 class InvoiceController extends BaseController
 {
@@ -57,5 +59,52 @@ class InvoiceController extends BaseController
 
         $app->close();
     }
+
+    public function pay()
+    {
+        $app = Factory::getApplication();
+        $input = $app->getInput();
+        $id = $input->getInt('id');
+
+        if (!$id) {
+            $app->enqueueMessage(Text::_('COM_MOTHERSHIP_ERROR_INVALID_INVOICE_ID'), 'error');
+            $this->setRedirect(Route::_('index.php?option=com_mothership&view=invoices', false));
+            return;
+        }
+
+        $model = $this->getModel('Invoice');
+        $invoice = $model->getItem($id);
+
+        if (!$invoice) {
+            $app->enqueueMessage(Text::_('COM_MOTHERSHIP_ERROR_INVOICE_NOT_FOUND'), 'error');
+            $this->setRedirect(Route::_('index.php?option=com_mothership&view=invoices', false));
+            return;
+        }
+
+        // Load enabled payment plugins
+        $plugins = \Joomla\CMS\Plugin\PluginHelper::getPlugin('mothership-payment');
+        $paymentOptions = [];
+
+        foreach ($plugins as $plugin) {
+            $params = new \Joomla\Registry\Registry($plugin->params);
+            $pluginName = $params->get('display_name') ?: ucfirst(str_replace('mothership-', '', $plugin->element));
+
+            $paymentOptions[] = [
+                'element'     => $plugin->element,
+                'name'        => $pluginName,
+                'fee_percent' => (float) $params->get('fee_percent', 0),
+                'fee_fixed'   => (float) $params->get('fee_fixed', 0),
+            ];
+        }
+
+        // Correct way to pass data to the view:
+        $view = $this->getView('Invoice', 'html');
+        $view->setModel($model, true);
+        $view->item = $invoice;
+        $view->paymentOptions = $paymentOptions;
+        $view->setLayout('payment');
+        $view->display();
+    }
+
 
 }
