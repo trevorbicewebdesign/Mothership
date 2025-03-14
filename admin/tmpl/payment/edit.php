cf74d152dabea1697a6b9debdf11a72a20f8c225
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-03-13 18:08:43 -0700

    Update edit.php

--- admin/tmpl/payment/edit.php
+++ admin/tmpl/payment/edit.php
@@ -1,10 +1,9 @@
 <?php
-
 /**
+ * Payment Edit Template for Mothership Component
+ *
  * @package     Joomla.Administrator
  * @subpackage  com_mothership
- *
- * @copyright   (C) 2009 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
@@ -15,7 +14,6 @@ use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Router\Route;
-use Joomla\CMS\Session\Session;
 
 JHtml::_('behavior.formvalidator');
 // JHtml::_('formbehavior.chosen', 'select');
@@ -27,36 +25,45 @@ $wa->useScript('table.columns')
     ->useScript('multiselect');
 
 $user = $this->getCurrentUser();
-$userId = $user->id;
-$listOrder = $this->escape($this->state->get('list.ordering'));
-$listDirn  = $this->escape($this->state->get('list.direction'));
 ?>
 
-<form action="<?php echo Route::_('index.php?option=com_mothership&layout=edit&id=' . (int) $this->item->id); ?>" method="post" name="adminForm" id="payment-form" aria-label="<?php echo Text::_('COM_MOTHERSHIP_PAYMENT_' . ((int) $this->item->id === 0 ? 'NEW' : 'EDIT'), true); ?>" class="form-validate">
+<form action="<?php echo Route::_('index.php?option=com_mothership&layout=edit&id=' . (int) $this->item->id); ?>"
+      method="post"
+      name="adminForm"
+      id="payment-form"
+      aria-label="<?php echo Text::_('COM_MOTHERSHIP_PAYMENT_' . ((int) $this->item->id === 0 ? 'NEW' : 'EDIT'), true); ?>"
+      class="form-validate">
     <div class="main-card">
-        <?php echo HTMLHelper::_('uitab.startTabSet', 'myTab', ['active' => 'details', 'recall' => true, 'breakpoint' => 768]); ?>
-        <?php echo HTMLHelper::_('uitab.addTab', 'myTab', 'details', Text::_('COM_MOTHERSHIP_FORM_PAYMENT_DETAILS_TAB')); ?>
+        <?php echo HTMLHelper::_('uitab.startTabSet', 'paymentTab', ['active' => 'details', 'recall' => true, 'breakpoint' => 768]); ?>
+        <?php echo HTMLHelper::_('uitab.addTab', 'paymentTab', 'details', Text::_('COM_MOTHERSHIP_FORM_PAYMENT_DETAILS_TAB')); ?>
         <div class="row">
-            <div class="col-lg-9">
-                <div>
-                    <fieldset class="adminform">
+            <div class="col-lg-8">
+                <fieldset class="adminform">
                     <?php echo $this->form->renderField('client_id'); ?>
-                    <?php echo $this->form->renderField('name'); ?>
-                    <?php echo $this->form->renderField('rate'); ?>                    
-                    </fieldset>
-                </div>
+                    <?php echo $this->form->renderField('account_id'); ?>
+                    <?php echo $this->form->renderField('amount'); ?>
+                    <?php echo $this->form->renderField('payment_date'); ?>
+                    <?php echo $this->form->renderField('fee_amount'); ?>
+                    <?php echo $this->form->renderField('fee_passed_on'); ?>
+                    <?php echo $this->form->renderField('payment_method'); ?>
+                    <?php echo $this->form->renderField('transaction_id'); ?>
+                </fieldset>
             </div>
-            <div class="col-lg-3">                
-                <?php echo $this->form->renderField('created'); ?>
+            <div class="col-lg-4">
+                <fieldset class="adminform">
+                    <?php echo $this->form->renderField('status'); ?>
+                    <?php echo $this->form->renderField('processed_date'); ?>
+                    <?php echo $this->form->renderField('created_at'); ?>
+                    <?php echo $this->form->renderField('updated_at'); ?>
+                </fieldset>
             </div>
         </div>
-
         <?php echo HTMLHelper::_('uitab.endTab'); ?>
         <?php echo HTMLHelper::_('uitab.endTabSet'); ?>
     </div>
 
-    <input type="hidden" name="jform[id]" value="<?php echo (isset($this->item->id) && $this->item->id > 0) ? (int) $this->item->id : ""; ?>" />
-    <input type="hidden" name="jform[return]" value="<?php echo $_REQUEST['return']; ?>" />
+    <input type="hidden" name="jform[id]" value="<?php echo (int) $this->item->id; ?>" />
+    <input type="hidden" name="jform[return]" value="<?php echo isset($_REQUEST['return']) ? $_REQUEST['return'] : ''; ?>" />
     <input type="hidden" name="task" value="" />
-    <?php echo JHtml::_('form.token'); ?>
+    <?php echo HTMLHelper::_('form.token'); ?>
 </form>
