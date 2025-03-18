6a60482b6c624c73805b05754a0056bef16b8dc2
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-03-17 17:26:11 -0700

    Save

--- admin/src/Model/PaymentsModel.php
+++ admin/src/Model/PaymentsModel.php
@@ -22,13 +22,20 @@ class PaymentsModel extends ListModel
     {
         if (empty($config['filter_fields'])) {
             $config['filter_fields'] = [
-                'id', 'p.id',
-                'payment_date', 'p.payment_date',
-                'amount', 'p.amount',
-                'payment_method', 'p.payment_method',
-                'client_name', 'c.name',
-                'checked_out', 'p.checked_out',
-                'checked_out_time', 'p.checked_out_time'
+                'id',
+                'p.id',
+                'payment_date',
+                'p.payment_date',
+                'amount',
+                'p.amount',
+                'payment_method',
+                'p.payment_method',
+                'client_name',
+                'c.name',
+                'checked_out',
+                'p.checked_out',
+                'checked_out_time',
+                'p.checked_out_time'
             ];
         }
         parent::__construct($config);
@@ -56,52 +63,51 @@ class PaymentsModel extends ListModel
     protected function getListQuery()
     {
         // Get a new query object.
-        $db    = $this->getDatabase();
+        $db = $this->getDatabase();
         $query = $db->getQuery(true);
 
         // Select the required fields from the payments table.
         $query->select(
             $this->getState(
-            'list.select',
-            [
-            $db->quoteName('p.id'),
-            $db->quoteName('p.client_id'),
-            $db->quoteName('p.account_id'),
-
-            $db->quoteName('c.name', 'client_name'),
-            $db->quoteName('a.name', 'account_name'),
-            $db->quoteName('p.amount'),
-            $db->quoteName('p.payment_method'),
-            $db->quoteName('p.payment_date'),
-            $db->quoteName('p.status'),
-            ]
+                'list.select',
+                [
+                    $db->quoteName('p.id'),
+                    $db->quoteName('p.client_id'),
+                    $db->quoteName('p.account_id'),
+                    $db->quoteName('c.name', 'client_name'),
+                    $db->quoteName('a.name', 'account_name'),
+                    $db->quoteName('p.amount'),
+                    $db->quoteName('p.payment_method'),
+                    $db->quoteName('p.payment_date'),
+                    $db->quoteName('p.status'),
+                ]
             )
         );
 
         // Use unique aliases for each table.
         $query->from($db->quoteName('#__mothership_payments', 'p'))
-              ->join('LEFT', $db->quoteName('#__mothership_clients', 'c') 
-                      . ' ON ' . $db->quoteName('p.client_id') . ' = ' . $db->quoteName('c.id'))
-              ->join('LEFT', $db->quoteName('#__mothership_accounts', 'a') 
-                      . ' ON ' . $db->quoteName('p.account_id') . ' = ' . $db->quoteName('a.id'));
+            ->join('LEFT', $db->quoteName('#__mothership_clients', 'c')
+                . ' ON ' . $db->quoteName('p.client_id') . ' = ' . $db->quoteName('c.id'))
+            ->join('LEFT', $db->quoteName('#__mothership_accounts', 'a')
+                . ' ON ' . $db->quoteName('p.account_id') . ' = ' . $db->quoteName('a.id'));
 
         // Filter by search term.
         if ($search = trim($this->getState('filter.search', ''))) {
             if (stripos($search, 'id:') === 0) {
                 $search = (int) substr($search, 4);
                 $query->where($db->quoteName('p.id') . ' = :search')
-                      ->bind(':search', $search, ParameterType::INTEGER);
+                    ->bind(':search', $search, ParameterType::INTEGER);
             } else {
                 $search = '%' . str_replace(' ', '%', $search) . '%';
                 // Since payments don't have a 'name', search on payment_method.
                 $query->where($db->quoteName('p.payment_method') . ' LIKE :search')
-                      ->bind(':search', $search);
+                    ->bind(':search', $search);
             }
         }
 
         // Add the ordering clause.
         $query->order(
-            $db->quoteName($db->escape($this->getState('list.ordering', 'p.payment_date'))) 
+            $db->quoteName($db->escape($this->getState('list.ordering', 'p.payment_date')))
             . ' ' . $db->escape($this->getState('list.direction', 'ASC'))
         );
 
@@ -169,5 +175,5 @@ class PaymentsModel extends ListModel
             return false;
         }
     }
-    
+
 }
