e93996f9542b39766916c2ce7bd13fc83c7014e8
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-04-03 18:48:00 -0700

    Update AccountModel.php

--- site/src/Model/AccountModel.php
+++ site/src/Model/AccountModel.php
@@ -20,37 +20,10 @@ class AccountModel extends BaseDatabaseModel
         // Load the account with status and related invoices
         $query = $db->getQuery(true)
             ->select([
-                'p.*',
-
-                // Interpreted status
-                'CASE ' . $db->quoteName('p.status') .
-                    ' WHEN 1 THEN ' . $db->quote('Pending') .
-                    ' WHEN 2 THEN ' . $db->quote('Completed') .
-                    ' WHEN 3 THEN ' . $db->quote('Failed') .
-                    ' WHEN 4 THEN ' . $db->quote('Cancelled') .
-                    ' WHEN 5 THEN ' . $db->quote('Refunded') .
-                    ' ELSE ' . $db->quote('Unknown') .
-                ' END AS status_text',
-
-                // Related invoice info
-                'inv.invoice_ids',
-                'inv.invoice_numbers'
+                'a.*',
             ])
-            ->from($db->quoteName('#__mothership_accounts', 'p'))
-
-            ->join(
-                'LEFT',
-                '(SELECT ip.account_id,
-                        GROUP_CONCAT(ip.invoice_id ORDER BY ip.invoice_id) AS invoice_ids,
-                        GROUP_CONCAT(i.number ORDER BY ip.invoice_id) AS invoice_numbers
-                FROM ' . $db->quoteName('#__mothership_invoice_account', 'ip') . '
-                JOIN ' . $db->quoteName('#__mothership_invoices', 'i') . ' ON ip.invoice_id = i.id
-                GROUP BY ip.account_id) AS inv
-                ON inv.account_id = p.id'
-            )
-
-            ->where('p.id = :id')
-            ->where('p.status != -1')
+            ->from($db->quoteName('#__mothership_accounts', 'a'))
+            ->where('a.id = :id')
             ->bind(':id', $id, \Joomla\Database\ParameterType::INTEGER);
 
         $db->setQuery($query);
