92ddc15b6557486b12650700e77a45fdb4d6d4a9
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-04-06 11:40:03 -0700

    Create default.php

---
+++ site/tmpl/projects/default.php
@@ -0,0 +1,46 @@
+<?php
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\Router\Route;
+use Joomla\CMS\Language\Text;
+
+?>
+<style>
+    .mt-4 {
+        margin-top: 1.5rem;
+    }
+</style>
+<h1>Projects</h1>
+<table class="table projectsTable " id="projectsTable">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Projects</th>
+            <th>Account</th>
+            <th>Registrar</th>
+            <th>Reseller</th>
+            <th>DNS</th>
+            <th>Created</th>
+            <th>Status</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php if(empty($this->projects)) : ?>
+            <tr>
+                <td colspan="8">No projects found.</td>
+            </tr>
+        <?php endif; ?>
+        <?php foreach ($this->projects as $projects) : ?>
+            <tr>
+                <td><?php echo $projects->id; ?></td>
+                <td><?php echo $projects->name; ?></td>
+                <td><?php echo $projects->account_name; ?></td>
+                <td><?php echo $projects->registrar; ?></td>
+                <td><?php echo $projects->reseller; ?></td>
+                <td><?php echo $projects->dns_provider; ?></td>
+                <td><?php echo $projects->created; ?></td>
+                <td><?php echo $projects->status; ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
