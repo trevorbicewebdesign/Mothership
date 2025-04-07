6f5b4e54187fd3d800c3618a1f5bfa1ac6720f37
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-04-07 01:36:20 -0700

    Update default.php

--- site/tmpl/projects/default.php
+++ site/tmpl/projects/default.php
@@ -15,19 +15,17 @@ use Joomla\CMS\Language\Text;
     <thead>
         <tr>
             <th>#</th>
-            <th>Projects</th>
+            <th>Name</th>
             <th>Account</th>
-            <th>Registrar</th>
-            <th>Reseller</th>
-            <th>DNS</th>
-            <th>Created</th>
+            <th>Type</th>
             <th>Status</th>
+            <th>Created</th>
         </tr>
     </thead>
     <tbody>
         <?php if(empty($this->projects)) : ?>
             <tr>
-                <td colspan="8">No projects found.</td>
+                <td colspan="6">No projects found.</td>
             </tr>
         <?php endif; ?>
         <?php foreach ($this->projects as $projects) : ?>
@@ -35,11 +33,9 @@ use Joomla\CMS\Language\Text;
                 <td><?php echo $projects->id; ?></td>
                 <td><a href="<?php echo Route::_("index.php?option=com_mothership&view=project&id={$projects->id}"); ?>"><?php echo $projects->name; ?></a></td>
                 <td><a href="<?php echo Route::_("index.php?option=com_mothership&view=account&id={$projects->account_id}"); ?>"><?php echo $projects->account_name; ?></a></td>
-                <td><?php echo $projects->registrar; ?></td>
-                <td><?php echo $projects->reseller; ?></td>
-                <td><?php echo $projects->dns_provider; ?></td>
-                <td><?php echo $projects->created; ?></td>
+                <td><?php echo $projects->type; ?></td>
                 <td><?php echo $projects->status; ?></td>
+                <td><?php echo $projects->created; ?></td>
             </tr>
         <?php endforeach; ?>
     </tbody>
