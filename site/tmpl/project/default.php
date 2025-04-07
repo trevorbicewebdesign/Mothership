791a56a9871822eec220f8bcd7b88b0f5d5f9e0c
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-04-06 19:40:46 -0700

    Update default.php

--- site/tmpl/project/default.php
+++ site/tmpl/project/default.php
@@ -13,41 +13,26 @@ $created = $project->created ? HTMLHelper::_('date', $project->created, Text::_(
 
 <div class="container my-4">
     <h1><?= $project->name ?></h1>
-
+    <p><div class="border rounded p-2 mt-1 bg-light">
+        <?= $project->description ?>
+    </div></p>
     <div class="card shadow-sm">
         <div class="card-body">
-            <div class="row mb-3">
-                <div class="col-md-6">
-                    <strong>Client ID:</strong> <?= (int) $project->client_id ?>
+            <div class="row mb-12">
+                <div class="col-md-12">
+                    <strong>Account</strong>
+                    <?= $project->account_name !== null ? $project->account_name : '<em>None</em>' ?>
                 </div>
-                <div class="col-md-6">
-                    <strong>Account ID:</strong>
-                    <?= $project->account_id !== null ? (int) $project->account_id : '<em>None</em>' ?>
-                </div>
-            </div>
-
-            <div class="row mb-3">
-                <div class="col-md-4">
+                <div class="col-md-12">
                     <strong>Status:</strong> <?= ucfirst($project->status) ?>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-12">
                     <strong>Type:</strong> <?= htmlspecialchars($project->type ?? '-') ?>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-12">
                     <strong>Created:</strong> <?= $created ?>
                 </div>
             </div>
-
-            <?php if (!empty($project->description)): ?>
-                <div class="row mb-3">
-                    <div class="col-12">
-                        <strong>Description:</strong>
-                        <div class="border rounded p-2 mt-1 bg-light">
-                            <?= nl2br(htmlspecialchars($project->description)) ?>
-                        </div>
-                    </div>
-                </div>
-            <?php endif; ?>
         </div>
     </div>
 </div>
