d26501b1f9987b3377e9dca77326cb8af49dd62f
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-04-09 15:24:41 -0700

    Update instructions.php

--- plugins/mothership-payment/paybycheck/tmpl/instructions.php
+++ plugins/mothership-payment/paybycheck/tmpl/instructions.php
@@ -6,8 +6,14 @@
  * @license     ...
  */
 
+
 // Protect against direct access
 defined('_JEXEC') or die;
+
+// get the plugin settings
+$plugin = JPluginHelper::getPlugin('mothership-payment', 'paybycheck');
+$pluginParams = new JRegistry($plugin->params);
+
 
 /** @var array $displayData */
 $invoiceId = $displayData['invoiceId'] ?? 0;
@@ -15,8 +21,9 @@ $invoiceId = $displayData['invoiceId'] ?? 0;
 <h1>Pay By Check Payment Instructions</h1>
 <p>Invoice ID: <?php echo (int) $invoiceId; ?></p>
 <p>
-  Please send payment via Zelle to <strong>payments@example.com</strong>.
+  Please write a check to <strong><?= $pluginParams['checkpayee']; ?></strong>.
 </p>
 <p>
-  After sending the payment, return to the site to confirm.
-</p>
+  After sending the payment please visit <a href="<?php echo JRoute::_('index.php?option=com_mothership&view=payments'); ?>">Payments</a><br/>
+  Your payment will have been set to `pending` until an administrator receives payment and can set the payment to `completed`.<br/>
+  <button type="button" onclick="alert('Payment Sent')">Payment Sent</button>
