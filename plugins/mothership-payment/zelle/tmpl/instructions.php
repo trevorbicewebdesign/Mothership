45ced8560861f3dd668c650ea86a47c96f28e449
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-04-09 12:09:54 -0700

    Update instructions.php

--- plugins/mothership-payment/zelle/tmpl/instructions.php
+++ plugins/mothership-payment/zelle/tmpl/instructions.php
@@ -9,14 +9,20 @@
 // Protect against direct access
 defined('_JEXEC') or die;
 
+// get the plugin settings
+$plugin = JPluginHelper::getPlugin('mothership-payment', 'zelle');
+$pluginParams = new JRegistry($plugin->params);
+
 /** @var array $displayData */
 $invoiceId = $displayData['invoiceId'] ?? 0;
 ?>
 <h1>Zelle Payment Instructions</h1>
 <p>Invoice ID: <?php echo (int) $invoiceId; ?></p>
 <p>
-  Please send payment via Zelle to <strong>payments@example.com</strong>.
+  Please send payment via Zelle to <strong><?php echo $pluginParams['zelle_email']; ?></strong>.
 </p>
 <p>
-  After sending the payment, return to the site to confirm.
+  After sending the payment please visit <a href="<?php echo JRoute::_('index.php?option=com_mothership&view=payments'); ?>">Payments</a><br/>
+  Your payment will have been set to `pending` until an administrator receives payment and can set the payment to `completed`.<br/>
+  <button type="button" onclick="alert('Payment Sent')">Payment Sent</button>
 </p>
