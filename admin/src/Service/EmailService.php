e17cb76c1ede6c2b21232a71a0ba355c9f3157f7
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-04-13 13:17:49 -0700

    Update EmailService.php

--- admin/src/Service/EmailService.php
+++ admin/src/Service/EmailService.php
@@ -8,40 +8,29 @@ use Joomla\CMS\Factory;
 use Joomla\CMS\Layout\FileLayout;
 use Joomla\CMS\Mail\Mail;
 
-/**
- * Service to send templated emails for Mothership
- */
 class EmailService
 {
     /**
-     * Sends an email using a Joomla layout template.
-     *
-     * @param string $template   The name of the layout file (no extension)
-     * @param string|array $to   Email address(es) to send to
-     * @param string $subject    Email subject
-     * @param array  $data       Data passed to layout
-     * @param array  $options    ['cc' => [], 'bcc' => []]
-     *
-     * @return boolean
+     * Sends a rendered email using a layout template.
      */
     public static function sendTemplate(string $template, $to, string $subject, array $data = [], array $options = []): bool
     {
-        
-        $htmlBody = self::renderLayout("emails.$template", $data);
-        $htmlBody = "This is a test";
-        
-        $textLayout = "emails.$template.text";
-        //$textBody = self::layoutExists($textLayout) ? self::renderLayout($textLayout, $data) : strip_tags($htmlBody);
-        $textBody = "This is a test";
-       
+        $body = self::generateBody($template, $data);
+        return self::sendEmail($to, $subject, $body, $options);
+    }
 
+    /**
+     * Sends a raw email with provided body content.
+     */
+    public static function sendEmail($to, string $subject, array $body, array $options = []): bool
+    {
         /** @var Mail $mailer */
         $mailer = Factory::getMailer();
         $mailer->addRecipient($to);
         $mailer->setSubject($subject);
         $mailer->isHtml(true);
-        $mailer->setBody($htmlBody);
-        $mailer->AltBody = $textBody;
+        $mailer->setBody($body['html'] ?? '');
+        $mailer->AltBody = $body['text'] ?? strip_tags($body['html'] ?? '');
 
         if (!empty($options['cc'])) {
             $mailer->addCc($options['cc']);
@@ -55,25 +44,29 @@ class EmailService
     }
 
     /**
-     * Renders a Joomla layout with given data
-     *
-     * @param string $layoutName Dot-separated layout path (e.g. 'emails.invoice')
-     * @param array  $data
-     *
-     * @return string
+     * Generates email body (html + text) from a layout
      */
+    public static function generateBody(string $template, array $data = []): array
+    {
+        $html = self::renderLayout("emails.$template", $data);
+
+        $textLayout = "emails.$template.text";
+        $text = self::layoutExists($textLayout)
+            ? self::renderLayout($textLayout, $data)
+            : strip_tags($html);
+
+        return [
+            'html' => $html,
+            'text' => $text
+        ];
+    }
+
     private static function renderLayout(string $layoutName, array $data): string
     {
         $layout = new FileLayout($layoutName, \JPATH_ROOT . '/administrator/components/com_mothership/layouts');
         return $layout->render($data);
     }
 
-    /**
-     * Checks if a layout file exists
-     *
-     * @param string $layoutName
-     * @return bool
-     */
     private static function layoutExists(string $layoutName): bool
     {
         $file = \JPATH_ROOT . '/administrator/components/com_mothership/layouts/' . str_replace('.', '/', $layoutName) . '.php';
