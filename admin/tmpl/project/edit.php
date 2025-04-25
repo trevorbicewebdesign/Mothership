61b02889568f550a2cbd6753d74b788a84cc7c28
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-04-25 10:57:07 -0700

    Update edit.php

--- admin/tmpl/project/edit.php
+++ admin/tmpl/project/edit.php
@@ -41,6 +41,8 @@ $listDirn  = $this->escape($this->state->get('list.direction'));
                 <div>
                     <fieldset class="adminform">
                     <?php echo $this->form->renderField('client_id'); ?>
+                    <?php echo $this->form->renderField('account_id'); ?>
+                    <?php echo $this->form->renderField('type'); ?>
                     <?php echo $this->form->renderField('name'); ?>
                     <?php echo $this->form->renderField('rate'); ?>
                    
