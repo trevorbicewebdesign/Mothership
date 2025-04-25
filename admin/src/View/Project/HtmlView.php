52479d4cbbe1d399f186c5ef1981c2bfa868cad1
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-04-25 14:31:37 -0700

    Update HtmlView.php

--- admin/src/View/Project/HtmlView.php
+++ admin/src/View/Project/HtmlView.php
@@ -150,8 +150,10 @@ class HtmlView extends BaseHtmlView
             }
         );
 
-        // Add custom toolbar button for WHOIS scan
-        ToolbarHelper::custom('project.mothershipScan', 'refresh', '', 'COM_MOTHERSHIP_PROJECT_SCAN_UPDATE', false);
+        if(!$isNew)
+        {
+            ToolbarHelper::custom('project.mothershipScan', 'refresh', '', 'COM_MOTHERSHIP_PROJECT_SCAN_UPDATE', false);
+        }
 
         if (empty($this->item->id)) {
             $toolbar->cancel('project.cancel', 'JTOOLBAR_CANCEL');
