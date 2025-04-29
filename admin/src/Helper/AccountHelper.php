de27a949b9d1731bed239c647d7651bd2ed5ed2e
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-04-29 09:47:29 -0700

    Update AccountHelper.php

--- admin/src/Helper/AccountHelper.php
+++ admin/src/Helper/AccountHelper.php
@@ -66,7 +66,7 @@ class AccountHelper extends ContentHelper
         $db = Factory::getContainer()->get(\Joomla\Database\DatabaseInterface::class);
 
         $query = $db->getQuery(true)
-            ->select('*')
+            ->select($db->quoteName('*', false))
             ->from($db->quoteName('#__mothership_accounts'))
             ->where($db->quoteName('id') . ' = ' . $db->quote($account_id));
 
