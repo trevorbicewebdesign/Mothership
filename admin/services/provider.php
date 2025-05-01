e9c706e3a17f2728f297895067389cefc58b957d
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-04-30 18:02:40 -0700

    Save

--- admin/services/provider.php
+++ admin/services/provider.php
@@ -14,7 +14,7 @@ use Joomla\DI\ServiceProviderInterface;
 
 require_once JPATH_ROOT . '/administrator/components/com_mothership/src/Field/ClientList.php';
 require_once JPATH_ROOT . '/administrator/components/com_mothership/src/Field/AccountList.php';
-require_once JPATH_ROOT . '/administrator/components/com_mothership/src/Field/InvoiceItems.php';
+require_once JPATH_ROOT . '/administrator/components/com_mothership/src/Field/invoiceitems.php';
 require_once JPATH_ROOT . '/administrator/components/com_mothership/src/Field/PaymentStatusField.php';
 
 require_once JPATH_ROOT . '/administrator/components/com_mothership/vendor/autoload.php';
