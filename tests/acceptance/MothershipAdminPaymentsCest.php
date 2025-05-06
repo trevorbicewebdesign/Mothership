48c021accfdfee3e8f6d364deb67c2564c6c751c
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-05-06 15:01:02 -0700

    Update MothershipAdminPaymentsCest.php

--- tests/acceptance/MothershipAdminPaymentsCest.php
+++ tests/acceptance/MothershipAdminPaymentsCest.php
@@ -97,8 +97,8 @@ class MothershipAdminPaymentsCest
         $I->fillField("input[name=username]", "admin");
         $I->fillField("input[name=passwd]", "password123!test");
         $I->click("Log in");
-        //$I->waitForText("Hide Forever");
-        //$I->click("Hide Forever");
+        $I->waitForText("Hide Forever");
+        $I->click("Hide Forever");
     }
 
     /**
