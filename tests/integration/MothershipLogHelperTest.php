99955cf15a109816cc3c642f72db7a20d0523d38
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-05-08 11:43:08 -0700

    Update MothershipLogHelperTest.php

--- tests/integration/MothershipLogHelperTest.php
+++ tests/integration/MothershipLogHelperTest.php
@@ -13,6 +13,8 @@ class MothershipLogHelperTest extends \Codeception\Test\Unit
     protected $accountData;
     protected $invoiceData;
     protected $paymentData;
+    protected $projectData;
+    protected $domainData;
 
     protected function _before()
     {
@@ -27,6 +29,20 @@ class MothershipLogHelperTest extends \Codeception\Test\Unit
         $this->accountData = $this->tester->createMothershipAccount([
             'client_id' => $this->clientData['id'],
             'name' => 'Test Account',
+        ]);
+
+        $this->projectData = $this->tester->createMothershipProject([
+            'client_id' => $this->clientData['id'],
+            'account_id' => $this->accountData['id'],
+            'name' => 'Test Project',
+            'type' => 'website',
+        ]);
+
+        $this->domainData = $this->tester->createMothershipDomain([
+            'client_id' => $this->clientData['id'],
+            'account_id' => $this->accountData['id'],
+            'project_id' => $this->projectData['id'],
+            'name' => 'google.com',
         ]);
 
         $this->invoiceData = $this->tester->createMothershipInvoice([
@@ -70,6 +86,61 @@ class MothershipLogHelperTest extends \Codeception\Test\Unit
             'object_id' => $this->invoiceData['id'],
             'action' => 'viewed',
             'user_id' => 1,
+        ]);
+    }
+
+    public function objectTypeProvider()
+    {
+        return [
+            'client' => ['client'],
+            'account' => ['account'],
+            'project' => ['project'],
+            'domain' => ['domain'],
+            'invoice' => ['invoice'],
+            'payment' => ['payment'],
+        ];
+    }
+
+    /**
+     * @dataProvider objectTypeProvider
+     */
+    public function testLogObjectViewed($objectType)
+    {
+        switch($objectType){
+            case 'invoice':
+                $objectId = $this->invoiceData['id'];
+                break;
+            case 'payment':
+                $objectId = $this->paymentData['id'];
+                break;
+            case 'project':
+                $objectId = $this->projectData['id'];
+                break;
+            case 'client':
+                $objectId = $this->clientData['id'];
+                break;
+            case 'account':
+                $objectId = $this->accountData['id'];
+                break;
+            case 'domain':
+                $objectId = $this->domainData['id'];
+                break;
+            default:
+                $this->fail("Invalid object type: $objectType");
+        }
+        codecept_debug("Logging object viewed: $objectType with ID: $objectId");
+        $result = LogHelper::logObjectViewed($objectType, $objectId, $this->clientData['id'], $this->accountData['id']);
+        codecept_debug($result);
+
+        $this->assertTrue($result, 'Log entry was not created successfully.');
+
+        $this->tester->seeInDatabase('jos_mothership_logs', [
+            'client_id' => $this->clientData['id'],
+            'account_id' => $this->accountData['id'],
+            'object_type' => $objectType,
+            'object_id' => $objectId,
+            'action' => 'viewed',
+            // 'user_id' => 1,
         ]);
     }
 }
