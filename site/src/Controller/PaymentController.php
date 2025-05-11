9301defa36705c751aac5a9853ade1d91631d878
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-05-11 10:58:39 -0700

    Update PaymentController.php

--- site/src/Controller/PaymentController.php
+++ site/src/Controller/PaymentController.php
@@ -80,51 +80,6 @@ class PaymentController extends BaseController
         $this->setRedirect(Route::_("index.php?option=com_mothership&view=payment&layout=thank-you&id={$payment->id}&invoice_id={$invoice_id}", false));
     }
 
-    public function payment()
-    {
-        $app = Factory::getApplication();
-        $input = $app->getInput();
-        $id = $input->getInt('id');
-
-        if (!$id) {
-            $app->enqueueMessage(Text::_('COM_MOTHERSHIP_ERROR_INVALID_PAYMENT_ID'), 'error');
-            $this->setRedirect(Route::_('index.php?option=com_mothership&view=payments', false));
-            return;
-        }
-
-        $model = $this->getModel('Payment');
-        $payment = $model->getItem($id);
-
-        if (!$payment) {
-            $app->enqueueMessage(Text::_('COM_MOTHERSHIP_ERROR_PAYMENT_NOT_FOUND'), 'error');
-            $this->setRedirect(Route::_('index.php?option=com_mothership&view=payments', false));
-            return;
-        }
-
-        // Load enabled payment plugins
-        $plugins = \Joomla\CMS\Plugin\PluginHelper::getPlugin('mothership-payment');
-        $paymentOptions = [];
-
-        foreach ($plugins as $plugin) {
-            $params = new \Joomla\Registry\Registry($plugin->params);
-            $pluginName = $params->get('display_name') ?: ucfirst(str_replace('mothership-', '', $plugin->element));
-
-            $paymentOptions[] = [
-                'element'     => $plugin->name,
-                'name'        => $pluginName,
-                'fee_amount'  => $plugin->getFee($invoice->total),
-                'display_fee' => $plugin->displayFee($invoice->total),
-            ];
-        }
-        // Correct way to pass data to the view:
-        $view = $this->getView('Payment', 'html');
-        $view->setModel($model, true);
-        $view->item = $payment;
-        $view->paymentOptions = $paymentOptions;
-        $view->setLayout('payment');
-        $view->display();
-    }
-
     protected function getPluginInstance(string $pluginName)
     {
         // Normalize plugin name casing
@@ -152,49 +107,4 @@ class PaymentController extends BaseController
 
         throw new \RuntimeException("Payment plugin '$pluginName' not found or not enabled. 1 ".json_encode($plugins));
     }
-
-    public function processPayment()
-    {
-        $app = Factory::getApplication();
-        $input = $app->getInput();
-        $id = $input->getInt('id');
-        $paymentMethod = $input->getCmd('payment_method');
-
-        if (!$id || !$paymentMethod) {
-            $app->enqueueMessage(Text::_('COM_MOTHERSHIP_ERROR_INVALID_PAYMENT_REQUEST'), 'error');
-            $this->setRedirect(Route::_('index.php?option=com_mothership&task=payment.payment&id=' . $id, false));
-            return;
-        }
-
-        $model = $this->getModel('Payment');
-        $payment = $model->getItem($id);
-
-        if (!$payment) {
-            $app->enqueueMessage(Text::_('COM_MOTHERSHIP_ERROR_PAYMENT_NOT_FOUND'), 'error');
-            $this->setRedirect(Route::_('index.php?option=com_mothership&view=payments', false));
-            return;
-        }
-
-
-        // Save the selected payment method
-        $payment->payment_method = $paymentMethod;
-        $payment->store(); // Persist it now
-
-        // Direct plugin invocation
-        try {
-            $plugin = $this->getPluginInstance($paymentMethod);
-
-            if (!method_exists($plugin, 'process')) {
-                throw new \RuntimeException("Plugin '$paymentMethod' does not support processing. 1",json_encode($plugin));
-            }
-
-            return $plugin->process($payment); // Let the plugin redirect or render
-        } catch (\Exception $e) {
-            $app->enqueueMessage(Text::sprintf('COM_MOTHERSHIP_PAYMENT_PROCESSING_FAILED', $e->getMessage()), 'error');
-            $this->setRedirect(Route::_('index.php?option=com_mothership&view=payments', false));
-            return;
-        }
-    }
-
-
 }
