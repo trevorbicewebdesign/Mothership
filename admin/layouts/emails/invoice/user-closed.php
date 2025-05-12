36c47f17dc44cefae8000c3336b25eeff667e6e1
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-05-12 14:33:19 -0700

    Update user-closed.php

--- admin/layouts/emails/invoice/user-closed.php
+++ admin/layouts/emails/invoice/user-closed.php
@@ -1,1 +1,5 @@
-Test User Invoice Closed
+<?php
+$fname = $displayData['fname'];
+?>
+<p>Hello <?= htmlspecialchars($fname, ENT_QUOTES, 'UTF-8'); ?>,</p>
+<p>Your invoice has been marked as closed.</p>
