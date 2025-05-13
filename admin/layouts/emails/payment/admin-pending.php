2c3d6a165fc3711af632ccc36ae7b9f2675c4b1a
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-05-13 13:04:46 -0700

    Update admin-pending.php

--- admin/layouts/emails/payment/admin-pending.php
+++ admin/layouts/emails/payment/admin-pending.php
@@ -13,7 +13,7 @@ $view_link = $displayData['view_link'] ?? '#';
 
 <p>A new <strong><?php echo $payment->payment_method; ?></strong> payment has been initiated by <strong><?php echo $client->name; ?></strong> for the amount of <strong><?php echo "$".number_format($invoice->amount, 2); ?></strong>.</p>
 
-<p>Please mark this payment as <code>Confirmed</code> once you have received and verified the check.</p>
+<p>Please mark this payment as <code>Confirmed</code> once you have received and verified the payment.</p>
 
 <p><a href="<?php echo $view_link; ?>" style="display:inline-block;padding:10px 15px;background:#007BFF;color:#fff;text-decoration:none;border-radius:4px;">Review Payment</a></p>
 
