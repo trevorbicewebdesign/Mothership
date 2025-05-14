85c7b6e4356a7988be9720ab5df3aa988dbfa2d4
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-05-14 12:55:59 -0700

    Update user-opened.php

--- admin/layouts/emails/invoice/user-opened.php
+++ admin/layouts/emails/invoice/user-opened.php
@@ -3,9 +3,12 @@
 defined('_JEXEC') or die;
 
 $fname = $displayData['fname'];
-$invoice_number = $displayData['invoice']->number;
+$invoice = $displayData['invoice'];
+$invoice_number = $invoice->number;
 $client_name = $displayData['client']->name;
+
 ?>
 <p>Hello <?php echo $fname; ?>,</p>
 <p>Invoice <strong>#<?php echo $invoice_number; ?></strong> for <strong>`<?php echo $client_name; ?>`</strong> is ready for your review. Please sign in to view
     details and make a payment. </p>
+<p>Please review, and settle the invoice before <strong><?php echo $invoice->due_date; ?></strong>.</p>
