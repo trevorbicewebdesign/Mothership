4c985f97cc547c9607527408074c94efaa6bef2b
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-05-14 15:47:40 -0700

    Update user-confirmed.php

--- admin/layouts/emails/payment/user-confirmed.php
+++ admin/layouts/emails/payment/user-confirmed.php
@@ -3,6 +3,8 @@
 defined('_JEXEC') or die;
 
 $fname = $displayData['fname'];
+$payment = $displayData['payment'];
 ?>
-<p>Hello <?= $fname; ?>,</p>
-<p>Your payment has been confirmed.</p>
+<p>Hello <?php echo $fname; ?>,</p>
+<p>Thank You!</p>
+<p>Your payment <?php echo "#".$payment->id; ?> for <?php echo "$".number_format($payment->amount, 2); ?> with payment method `<?php echo $payment->payment_method; ?>` has been received and confirmed by an admin.</p>
