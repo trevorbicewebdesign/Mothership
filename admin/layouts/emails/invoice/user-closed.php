858b3f942711a9c31e886d7c3bc70f5437ff6867
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-05-14 13:23:37 -0700

    Update user-closed.php

--- admin/layouts/emails/invoice/user-closed.php
+++ admin/layouts/emails/invoice/user-closed.php
@@ -3,7 +3,9 @@
 defined('_JEXEC') or die;
 
 $fname = $displayData['fname'];
+$invoice = $displayData['invoice'];
+$client = $displayData['client'];
 
 ?>
 <p>Hello <?= htmlspecialchars($fname, ENT_QUOTES, 'UTF-8'); ?>,</p>
-<p>Your invoice has been marked as closed.</p>
+<p>Thank you for your payment, invoice #<?php echo $invoice->number; ?> has been marked as closed.</p>
