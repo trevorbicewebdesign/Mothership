cd813121f34ae65d4c21be804a53ef58c321624a
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-05-14 22:21:50 -0700

    Update default.php

--- site/tmpl/invoice/default.php
+++ site/tmpl/invoice/default.php
@@ -2,8 +2,14 @@
 \defined('_JEXEC') or die;
 
 use Joomla\CMS\Layout\FileLayout;
+use Mothership\Component\Mothership\Site\Helper\ClientHelper;
+use Mothership\Component\Mothership\Site\Helper\AccountHelper;
+use Mothership\Component\Mothership\Site\Helper\MothershipHelper;
 
 $invoice = isset($this->item) ? $this->item : null;
+$client = ClientHelper::getClient($invoice->client_id);
+$account = AccountHelper::getAccount($invoice->account_id);
+$business = MothershipHelper::getMothesrshipOptions();
 
 if (!$invoice) {
     echo '<div class="alert alert-warning">Invoice not found.</div>';
@@ -11,4 +17,9 @@ if (!$invoice) {
 }
 
 $layout = new FileLayout('pdf', JPATH_ROOT . '/components/com_mothership/layouts');
-echo $layout->render(['invoice' => $invoice]);
+echo $layout->render([
+    'invoice' => $invoice,
+    'client' => $client,
+    'account' => $account,
+    'business' => $business
+]);
