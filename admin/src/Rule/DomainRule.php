8473c964843320de5f9a2812a64e73f7ca30fbeb
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-05-15 14:53:16 -0700

    Delete DomainRule.php

--- admin/src/Rule/DomainRule.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace TrevorBice\Component\Mothership\Administrator\Rule;
-
-use Joomla\CMS\Form\FormRule; 
-
-class CustomRule extends FormRule
-{
-    protected $regex = '^[0-9\*\#]+$';
-}
