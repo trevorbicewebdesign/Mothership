79f5212c6da241f33587c4c29c19d31060474e5f
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-06-03 12:41:45 -0700

    Update MothershipEmailServiceTest.php

--- tests/integration/MothershipEmailServiceTest.php
+++ tests/integration/MothershipEmailServiceTest.php
@@ -142,15 +142,5 @@ class MothershipEmailServiceTest extends \Codeception\Test\Unit
         $email_id = $this->tester->getEmailBySubject("New Pending Payment for Pay By Check");        
         $email = $this->tester->getEmailById($email_id);
         $this->assertEquals($email['Subject'], "New Pending Payment for Pay By Check", 'Email subject does not match.');
-
-        /*
-        $this->tester->seeInDatabase('jos_mothership_logs', [
-            'client_id' => $this->clientData['id'],
-            'account_id' => $this->accountData['id'],
-            'object_type' => 'invoice',
-            'object_id' => $this->invoiceData['id'],
-            'action' => 'status_opened',
-        ]);
-        */
     }
 }
