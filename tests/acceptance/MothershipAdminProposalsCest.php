76132a9f719416e8368e6029e83ce494b151f82d
Trevor Bice <trevorbicewebdesign@gmail.com>
2025-12-11 19:27:53 -0800

    Update MothershipAdminProposalsCest.php

--- tests/acceptance/MothershipAdminProposalsCest.php
+++ tests/acceptance/MothershipAdminProposalsCest.php
@@ -415,9 +415,9 @@ class MothershipAdminProposalsCest
         $I->seeNumberOfElements("#j-main-container table.itemList tbody tr", 2);
 
         $I->see("1001", "#j-main-container table.itemList tbody tr td:nth-child(3)");
-        $I->see("Test Client", "#j-main-container table.itemList tbody tr td:nth-child(5)");
-        $I->see("Test Account", "#j-main-container table.itemList tbody tr td:nth-child(6)");
-        $I->see(date("Y-m-d"), "#j-main-container table.itemList tbody tr td:nth-child(11)");
+        $I->see("Test Client", "#j-main-container table.itemList tbody tr td:nth-child(6)");
+        $I->see("Test Account", "#j-main-container table.itemList tbody tr td:nth-child(7)");
+        $I->see(date("Y-m-d"), "#j-main-container table.itemList tbody tr td:nth-child(12)");
 
         // Open the Proposal again and confirm the data is correct
         $I->amOnPage(sprintf(self::PROPOSAL_EDIT_URL, ($this->proposalData['id'] + 1)));
